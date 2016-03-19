dd3241f450dbaace686eedf7d30e7dc307fd9e0a
ARDivekar <abhishek.r.divekar@gmail.com>
2016-03-19 14:04:14 +0530

    v1.1 - Added formatting tables to database scripts.
    The DB scripts (in /DB_script/) are now shown in a tabluar manner, with
    the relevant details mentioned.
    The order to execute is: Drop, Generate, Populate.
    Calling Populate many times may cause multiple entries in the  tables,
    so call it only once.

--- DB_script/Spons Database Drop Tables.php
+++ DB_script/Spons Database Drop Tables.php
@@ -1,45 +1,84 @@
 <?php 
 
 require('DBconnect.php'); //imports all the connection settings.
+$dbname = $db_name;
+$result = mysql_list_tables($dbname);
+
+mysql_query('SET foreign_key_checks = 0');
+
+  
+if (!$result) {
+    print "DB Error, could not list tables\n";
+    print 'MySQL Error: ' . mysql_error();
+    exit;
+}
+echo "<h1> Dropping tables...</h1>";
+echo "<p align=\"left\">";
+echo "<table width=\"50%\" border=\"0\">";
+echo  "<tr bgcolor=\"#993333\"> ";
+echo    "<td width=\"10%\"><font face=\"Verdana, Arial, Helvetica, sans-serif\" size=\"-1\" color=\"#FFFFFF\">No.</font></td>";
+echo    "<td width=\"25%\"><font face=\"Verdana, Arial, Helvetica, sans-serif\" size=\"-1\" color=\"#FFFFFF\">Table name:</font></td>";
+echo    "<td width=\"65%\"><font face=\"Verdana, Arial, Helvetica, sans-serif\" size=\"-1\" color=\"#FFFFFF\">Status:</font></td>";
+echo  "</tr>";
+
+$i = 1;
+while ($row = mysql_fetch_row($result)) {
+    echo "<tr bgcolor=\"#CCCCCC\">";
+    echo    "<td>";
+       print "$i";
+    $i = $i +1;
+	echo    "</td>";
+
+	echo    "<td>";
+       print "$row[0]\n";
+	echo    "</td>";
+	$drop_query = "DROP TABLE $row[0]";
+	// echo $drop_query;
+	$deleteIt=mysql_query($drop_query); 
+	$status = "";
+	if($deleteIt){
+		$status="The table $row[0] has been deleted successfully.";
+	}
+	else{
+		$status="Table $row[0] has not been deleted. <br>An error has occured...please try again.";
+	}
+
+	echo    "<td>";	
+    print "$status\n";
+	echo    "</td>";
+	echo "</tr>";     
+}
+
+mysql_query('SET foreign_key_checks = 1');
 
 
+
+
+
+/*
 $all_table_names=array(
-
-	1 => "Festival",
-	2 => "CommitteeMember", 
-	3 => "AccountLog",
-	4 => "SponsRep", 
-	5 => "SectorHead", 
-	6 => "SponsLogin", 
-	7 => "Company", 
-	8 => "CompanyExec", 
-	9 => "Meeting"
-);//, "AccountLog", "Festival");
+	"Event",
+	"CommitteeMember", 
+	"AccountLog",
+	"SponsRep", 
+	"SectorHead", 
+	"SponsLogin", 
+	"Company", 
+	"CompanyExec", 
+	"Meeting"
+);
 
 
 //we are connecting with MYSQLI PROCEDURAL, __not__ object oriented.
 
-$i=1;
-while ($i <= 9){
-
+$i=0;
+while ($i < count($all_table_names)) {
 	$table_name=$all_table_names[$i]; 
 	
-	//echo "<br><br>" .$table_name.$table_args;
-
-	/* //Note: instead of the following code, we could also have used:
-
-		$query="select * from `$table_name`";
-		$result=mysql_query(  $query);
-		if(mysql_num_rows($result)>0){
-			//table has at least one row in it.
-		}	
-		
-	*/
-
 	$query="select 1 from `$table_name`"; //select 1 selects anything
-	if(mysql_query(  $query)){   
-		//DO SOMETHING! IT EXISTS!
-		echo "<br>Table $table_name exists. Destroying it.";
+	if(mysql_query($query)){   
+		//the table exists.
+		echo "<br><br>Table $table_name exists.";
 
 		if(mysql_query( "DROP TABLE `". $table_name."`")){
 			echo "<br>Table $table_name dropped successfully<br><br>";
@@ -49,6 +88,6 @@ while ($i <= 9){
 
 	$i=$i+1;
 }
-
+*/
 
 ?>
--- DB_script/Spons Database Generate Tables.php
+++ DB_script/Spons Database Generate Tables.php
@@ -3,66 +3,62 @@
 
 require('DBconnect.php');
 
-
-$all_table_names=array(
-
-	1 => "Festival",
-	2 => "CommitteeMember", 
-	3 => "AccountLog",
-	4 => "SponsRep", 
-	5 => "SectorHead", 
-	6 => "SponsLogin", 
-	7 => "Company", 
-	8 => "CompanyExec", 
-	9 => "Meeting"
-);//, "AccountLog", "Festival");
-
-
-
-$Festival_args = " (
-	FestName 		VARCHAR(20) PRIMARY KEY,
-	StartDate 		DATE,
-	EndDate 		DATE
+// The Sponsorship Department using the software has many events. They assign a subset of their CommitteeMembers to these events.
+// 'Technovanza' is an event, if funding is done at the level of Technovanza. The SponsorshipOrganization could be 'Technovanza'
+//'RoboWars' is an event, if funding is done for RoboWars. The SponsorshipOrganization could be 'Technovanza'
+$Event_args = " (
+	EventID						INT(7) UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
+	SponsorshipOrganization		VARCHAR(50),
+	EventName 					VARCHAR(80),
+	StartDate 					DATE,
+	EndDate 					DATE
 );";
 
+// A CommitteeMember is assigned to only one event at once.
 $CommitteeMember_args =" (
-	StudID 		INT(9) UNSIGNED PRIMARY KEY,
-	Name 		VARCHAR(50) NOT NULL,
-	Dept 		VARCHAR(15) NOT NULL,
-	Role 		VARCHAR(40) NOT NULL,
-	Mobile 		VARCHAR(15),
-	Email 		VARCHAR(50),
-	Year 		INT (1),
-	Branch 		VARCHAR(25),
-	
-);";
-
-$AccountLog_args = " (
-	Title 		VARCHAR(100) PRIMARY KEY NOT NULL,
-	SponsID 	INT(9) UNSIGNED NOT NULL,
-	Amount 		INT(8) UNSIGNED NOT NULL,
-	TransType 	ENUM ('Deposit','Withdraw') NOT NULL,
-	Date 		DATE,
-	foreign key (SponsID) References CommitteeMember(StudID)
+	ID 				INT(9) UNSIGNED PRIMARY KEY,
+	EventID			INT(7) UNSIGNED,
+	Name 			VARCHAR(80) NOT NULL,
+	Department		VARCHAR(15) NOT NULL,
+	Role 			VARCHAR(40) NOT NULL,
+	Mobile 			VARCHAR(15),
+	Email 			VARCHAR(50),
+	Year 			INT (1),
+	Branch 			VARCHAR(25),
+	foreign key (EventID) References Event(EventID)
 		On Update Cascade
 		On Delete Cascade
 );";
 
-//SponsID is the same as StudID
+$AccountLog_args = " (
+	ID 			INT(9) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	EventID		INT(7) UNSIGNED NOT NULL,
+	Title 		VARCHAR(100) NOT NULL,
+	SponsID 	INT(9) UNSIGNED NOT NULL,
+	Amount 		INT(8) UNSIGNED NOT NULL,
+	TransType 	ENUM ('Deposit','Withdraw') NOT NULL,
+	Date 		DATE,
+	foreign key (SponsID) References CommitteeMember(ID)
+		On Update Cascade
+		On Delete Cascade,
+	foreign key (SponsID) References CommitteeMember(ID)
+		On Update Cascade
+		On Delete Cascade
+);";
 
 $SponsRep_args=" (
 	SponsID 		INT(9) UNSIGNED  PRIMARY KEY,
 	Sector 			VARCHAR(15) NOT NULL,
 	DateAssigned 	DATE,
-	Foreign key (SponsID) References CommitteeMember(StudID)
+	Foreign key (SponsID) References CommitteeMember(ID)
 		On Update Cascade
 		On Delete Cascade
 	);";
 
 $SectorHead_args=" (
-	SponsID 	INT(9) UNSIGNED  PRIMARY KEY,
+	SponsID 	INT(9) UNSIGNED PRIMARY KEY,
 	Sector 		VARCHAR(15) NOT NULL,
-	foreign key (SponsID) References CommitteeMember(StudID)
+	foreign key (SponsID) References CommitteeMember(ID)
 		On Update Cascade
 		On Delete Cascade
 	);";
@@ -71,7 +67,7 @@ $SponsLogin_args=" (
 	SponsID 		INT(9) UNSIGNED PRIMARY KEY NOT NULL,
 	Password 		VARCHAR (20) NOT NULL,
 	AccessLevel 	ENUM ('SponsRep', 'SectorHead', 'CSO') NOT NULL,
-	foreign key (SponsID) References CommitteeMember(StudID)
+	foreign key (SponsID) References CommitteeMember(ID)
 		On Update Cascade
 		On Delete Cascade
 	);";
@@ -98,6 +94,7 @@ $CompanyExec_args=" (
 	);";
 
 $Meeting_args=" (
+	ID 				INT(7) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	Date 			DATE,
 	Time 			TIME,
 	SponsID 		INT(9) UNSIGNED,
@@ -106,8 +103,7 @@ $Meeting_args=" (
 	CMPName 		VARCHAR(100),
 	Outcome 		TEXT,
 	Address 		TEXT,
-	PRIMARY KEY 	(Date, Time, SponsID),
-	foreign key (SponsID) References CommitteeMember(StudID)
+	foreign key (SponsID) References CommitteeMember(ID)
 		On Update Cascade
 		On Delete Cascade,
 	foreign key (CMPName) References Company(CMPName)
@@ -120,46 +116,70 @@ $Meeting_args=" (
 );";
 
 
-
-$all_table_args=array(
-	1 => $Festival_args,
-	2 => $CommitteeMember_args, 
-	3 => $AccountLog_args,
-	4 => $SponsRep_args, 
-	5 => $SectorHead_args, 
-	6 => $SponsLogin_args, 
-	7 => $Company_args, 
-	8 => $CompanyExec_args, 
-	9 => $Meeting_args
+$all_tables=array(
+	array("Event", $Event_args),
+	array("CommitteeMember", $CommitteeMember_args), 
+	array("AccountLog", $AccountLog_args),
+	array("SponsRep", $SponsRep_args), 
+	array("SectorHead", $SectorHead_args), 
+	array("SponsLogin", $SponsLogin_args), 
+	array("Company", $Company_args), 
+	array("CompanyExec", $CompanyExec_args), 
+	array("Meeting", $Meeting_args)
 );  
 
-/*
-array_keys()
-for (array_keys($array_one) as $key) {
-    // do something with $array_one[$key] and $array_two[$key]
-}*/
 
-$i=1;
-while ($i <= 9){
+echo "<h1> Generating tables...</h1>";
+echo "<p align=\"left\">";
+echo "<table width=\"100%\" border=\"0\">";
+echo  "<tr bgcolor=\"#993333\"> ";
+echo    "<td width=\"5%\"><font face=\"Verdana, Arial, Helvetica, sans-serif\" size=\"-1\" color=\"#FFFFFF\">No.</font></td>";
+echo    "<td width=\"15%\"><font face=\"Verdana, Arial, Helvetica, sans-serif\" size=\"-1\" color=\"#FFFFFF\">Table name:</font></td>";
+echo    "<td width=\"25%\"><font face=\"Verdana, Arial, Helvetica, sans-serif\" size=\"-1\" color=\"#FFFFFF\">Status:</font></td>";
+echo    "<td width=\"55%\"><font face=\"Verdana, Arial, Helvetica, sans-serif\" size=\"-1\" color=\"#FFFFFF\">SQL code:</font></td>";
+echo  "</tr>";
 
-	$table_name=$all_table_names[$i];
-	$table_args=$all_table_args[$i]; 
-	//echo "<br><br>" .$table_name.$table_args;
+
+
+$i=0;
+while ($i < count($all_tables)){
+	echo "<tr bgcolor=\"#CCCCCC\">";
+	
+	$table_name=$all_tables[$i][0];
+	$table_args=$all_tables[$i][1]; 
+	echo    "<td>";
+       print $i+1;
+	echo    "</td>";
+	echo    "<td>";
+       print "$table_name\n";
+	echo    "</td>";
 
 	$query="select 1 from `$table_name`";
-
+	$status = "";
 	if(mysql_query( $query)){  
-		//DO SOMETHING! IT EXISTS!
-		echo "<br>Table $table_name exists. Truncating it.";
+		$status = $status."Table $table_name exists and may have data. Truncating it.";
 
 		if(mysql_query( "TRUNCATE TABLE ". $table_name)){
-			echo "<br>Table $table_name truncated (i.e. removed all data) successfully<br><br>";
+			$status = $status."<br>Table $table_name truncated (i.e. removed all data) successfully.";
 		}
 	}
-
-	if(mysql_query( "CREATE TABLE IF NOT EXISTS `".$table_name."`".$table_args) ) {
-		echo "<br>Successfully created table $table_name <br><br>";
+	$create_query = "CREATE TABLE IF NOT EXISTS `".$table_name."`".$table_args;
+	
+	if(mysql_query( $create_query) ) {
+		$status = $status."<br><b>Successfully created table $table_name.</b>";
 	}
+	else{
+		$status = $status."<br><b>Could not create table $table_name.</b>";	
+	}
+
+	echo    "<td>";	
+    print "$status\n";
+	echo    "</td>";
+
+	echo    "<td>";	
+    print "$create_query\n";
+	echo    "</td>";
+	echo "</tr>";
 
 	$i=$i+1;
 }
