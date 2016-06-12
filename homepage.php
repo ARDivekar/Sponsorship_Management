d58246da77dffb88769ce8d016312684c1108f51
Janit Mehta <janithmehta@gmail.com>
2016-06-12 21:03:47 +0530

    Added Graph on Homepage

--- homepage.php
+++ homepage.php
@@ -224,188 +224,52 @@
 				</div>
 			</div>
 		</div>
-		<!-- /.row -->
+
 		<div class="row">
 			<div class="col-lg-8">
 				<div class="panel panel-default">
 					<div class="panel-heading">
-						<i class="fa fa-bar-chart-o fa-fw"></i> Graph Showing Progress of Money
-						<div class="pull-right">
-							<div class="btn-group">
-								<button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
-									Actions
-									<span class="caret"></span>
-								</button>
-								<ul class="dropdown-menu pull-right" role="menu">
-									<li><a href="#">Action</a>
-									</li>
-									<li><a href="#">Another action</a>
-									</li>
-									<li><a href="#">Something else here</a>
-									</li>
-									<li class="divider"></li>
-									<li><a href="#">Separated link</a>
-									</li>
-								</ul>
-							</div>
-						</div>
+						<i class="fa fa-bar-chart-o fa-fw"></i>Money Progress
 					</div>
-					<!-- /.panel-heading -->
 					<div class="panel-body">
 						<div id="money-progress"></div>
-						<!--<?php 
-						/*	$MoneyProgress= $db->select("SELECT SUM(Amount) as 'Amount', MONTH(Date) as 'Month' FROM `accountlog` GROUP BY MONTH(Date)");
-							echo $MoneyProgress[0]['Count'];
+						<script>
+							
+							new Morris.Line({
+							  element: 'money-progress', 
+							  data: <?php 
+							  $MoneyProgress= $db->select("SELECT SUM(Amount) as 'amount', MONTH(Date) as 'month' FROM `accountlog` GROUP BY MONTH(Date)");
+							  echo json_encode($MoneyProgress);
+							  ?>,
+							  xkey: ['month'],
+							  ykeys: ['amount'],
+							  labels: ['Amount']
+							});
 
-							for ($x = 0; $x <= $MoneyProgress.length; $x++) {
-								$array = (
-									$MoneyProgress[i][]
-
-								)    
-							} 
-							echo "<script>
-
-									new Morris.Line({
-									  // ID of the element in which to draw the chart.
-									  element: 'money-progress',
-									  // Chart data records -- each entry in this array corresponds to a point on
-									  // the chart. " + "
-									  data: [
-									    { year: '2008', value: 20 },
-									    { year: '2009', value: 10 },
-									    { year: '2010', value: 5 },
-									    { year: '2011', value: 5 },
-									    { year: '2012', value: 20 }
-									  ],
-									  // The name of the data record attribute that contains x-values.
-									  xkey: 'month',
-									  // A list of names of data record attributes that contain y-values.
-									  ykeys: ['amount'],
-									  // Labels for the ykeys -- will be displayed when you hover over the
-									  // chart.
-									  labels: ['Amount']
-									});
-
-								</script>" */
-						?> -->
+						</script> 
 					</div>
-					<!-- /.panel-body -->
 				</div>
-				<!-- /.panel -->
-				<!-- <div class="panel panel-default">
-					<div class="panel-heading">
-						<i class="fa fa-bar-chart-o fa-fw"></i> Bar Chart Example
-						<div class="pull-right">
-							<div class="btn-group">
-								<button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
-									Actions
-									<span class="caret"></span>
-								</button>
-								<ul class="dropdown-menu pull-right" role="menu">
-									<li><a href="#">Action</a>
-									</li>
-									<li><a href="#">Another action</a>
-									</li>
-									<li><a href="#">Something else here</a>
-									</li>
-									<li class="divider"></li>
-									<li><a href="#">Separated link</a>
-									</li>
-								</ul>
-							</div>
-						</div>
-					</div> -->
-					<!-- /.panel-heading -->
-					<!-- <div class="panel-body">
-						<div class="row">
-							<div class="col-lg-4">
-								<div class="table-responsive">
-									<table class="table table-bordered table-hover table-striped">
-										<thead>
-										<tr>
-											<th>#</th>
-											<th>Date</th>
-											<th>Time</th>
-											<th>Amount</th>
-										</tr>
-										</thead>
-										<tbody>
-										<tr>
-											<td>3326</td>
-											<td>10/21/2013</td>
-											<td>3:29 PM</td>
-											<td>$321.33</td>
-										</tr>
-										<tr>
-											<td>3325</td>
-											<td>10/21/2013</td>
-											<td>3:20 PM</td>
-											<td>$234.34</td>
-										</tr>
-										<tr>
-											<td>3324</td>
-											<td>10/21/2013</td>
-											<td>3:03 PM</td>
-											<td>$724.17</td>
-										</tr>
-										<tr>
-											<td>3323</td>
-											<td>10/21/2013</td>
-											<td>3:00 PM</td>
-											<td>$23.71</td>
-										</tr>
-										<tr>
-											<td>3322</td>
-											<td>10/21/2013</td>
-											<td>2:49 PM</td>
-											<td>$8345.23</td>
-										</tr>
-										<tr>
-											<td>3321</td>
-											<td>10/21/2013</td>
-											<td>2:23 PM</td>
-											<td>$245.12</td>
-										</tr>
-										<tr>
-											<td>3320</td>
-											<td>10/21/2013</td>
-											<td>2:15 PM</td>
-											<td>$5663.54</td>
-										</tr>
-										<tr>
-											<td>3319</td>
-											<td>10/21/2013</td>
-											<td>2:13 PM</td>
-											<td>$943.45</td>
-										</tr>
-										</tbody>
-									</table>
-								</div>
-					 -->			<!-- /.table-responsive -->
-							</div>
-							<!-- /.col-lg-4 (nested) -->
-					<!-- 		<div class="col-lg-8">
-								<div id="morris-bar-chart"></div>
-							</div>
-					 -->		<!-- /.col-lg-8 (nested) -->
-						</div>
-						<!-- /.row -->
-					</div>
-					<!-- /.panel-body -->
-				</div>
-				<!-- /.panel -->
-
 			</div>
-			<!-- /.col-lg-8 -->
+
+			<div class="col-lg-4">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<i class="fa fa-calender-o fa-fw"></i>Meetings Scheduled
+					</div>
+					<div class="panel-body">
+						<?php 
+							$LatestMeetings = $db->select("SELECT Date, Time, MeetingType as 'Type', CMPName as 'Company' FROM `meeting` ORDER BY 'Date' LIMIT 5 WHERE ")
+
+						?>
+					</div>
+				</div>
+			</div>
+			</div>
 
 		</div>
-		<!-- /.row -->
+
 	</div>
-	<!-- /#page-wrapper -->
-
 </div>
-<!-- /#wrapper -->
-
 
 </body>
 
