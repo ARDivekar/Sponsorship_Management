e783a2346fa2b4ccc1344ea17122110e29b0a829
Abhishek Divekar <abhishek.r.divekar@gmail.com>
2017-02-16 21:39:35 +0530

    Made a terrible function

--- DBconnectTest.php
+++ DBconnectTest.php
@@ -3,10 +3,15 @@
   ini_set('display_errors', 1);
 
   include_once("DBconnect.php");
+  include_once("library_functions.php")
   $db = new SponsorshipDB();
 
-  if($db->isConnected())
-  	echo "CONNECTED TO MySQL DB";
+  if($db->isConnected()){
+    echo "CONNECTED TO MySQL DB";
+    echo_1d_array(parse_url(getenv("CLEARDB_DATABASE_URL")));
+  }
   else echo "COULD NOT CONNECT TO MySQL DB";
+
+
 
 ?>
