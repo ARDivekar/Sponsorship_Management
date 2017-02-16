a909d65948663406f71bf9ecfb5460d03b3fa7a6
Abhishek Divekar <abhishek.r.divekar@gmail.com>
2017-02-16 21:15:25 +0530

    Checking errors

--- DBconnectTest.php
+++ DBconnectTest.php
@@ -1,7 +1,10 @@
 <?php
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+
   include_once("DBconnect.php");
   $db = new SponsorshipDB();
-  
+
   if($db->isConnected())
   	echo "CONNECTED TO MySQL DB";
   else echo "COULD NOT CONNECT TO MySQL DB";
