01a86a4ff2e8f55604962f34bfe82075c08d48ac
Abhishek Divekar <abhishek.r.divekar@gmail.com>
2017-02-18 11:13:42 +0530

    Trying to enforce version 5.6

--- reportico/swutil.php
+++ reportico/swutil.php
@@ -1450,7 +1450,7 @@ function reformatDate($informat, $outformat, $date) {
 function column_name_to_label($columnname)
 {
 	$retstring = str_replace("_", " ", $columnname);
-	$retstring = ucwords(strtolower($retstring));
+	$retstring = ucwords(mb_strtolower($retstring,output_charset_to_php_charset(SW_OUTPUT_ENCODING)));
 	return $retstring;
 }
 
