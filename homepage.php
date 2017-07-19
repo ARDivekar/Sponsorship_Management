6e5ced151bc879690b6a4380eb7440bf203f9b5a
Abhishek Divekar <abhishek.r.divekar@gmail.com>
2017-07-20 00:32:05 +0530

    Testing output

--- homepage.php
+++ homepage.php
@@ -238,13 +238,13 @@
 
 							new Morris.Line({
 							  element: 'money-progress',
-							  data: <?php
+							  data: (<?php
 							  $MoneyProgress= $db->select("SELECT SUM(Amount) as 'Amount', WEEK(Date) as 'Week' FROM `AccountLog` GROUP BY WEEK(Date)");
 							  if(count($MoneyProgress)>0)
 							  	echo json_encode($MoneyProgress);
 							  else echo "<h1>Graph Unavailiable</h1>";
-							  ?>,
-							  xkey: ['week'],
+							  ?>),
+							  xkey: ['Week'],
 							  ykeys: ['Amount'],
 							  labels: ['Amount']
 							});
